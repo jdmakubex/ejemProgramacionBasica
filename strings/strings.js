554cb32c149baea9ef567b3d478adf6269537abb
Juan de Dios Marcos <marg.jd@gmail.com>
2016-01-26 22:24:54 -0600

    Strings

---
+++ strings/strings.js
@@ -0,0 +1,18 @@
+/*STRINGS
+Podemos tratar los strings parecido a un arreglo...
+por ejemplo:
+*/
+
+var palabra = Octubre;
+
+//Esto me permite convertir a mayusculas todos los caracteres contenidos en palabra
+palabra.toUpperCase();
+
+alert(palabra[5]);
+
+//lo anterior me devuelve en un aler "R"
+
+
+//nos davuelve el numero de caracteres contenidos en palabra
+//No necesita (), por que es una variable no un metodo.
+palabra.length
